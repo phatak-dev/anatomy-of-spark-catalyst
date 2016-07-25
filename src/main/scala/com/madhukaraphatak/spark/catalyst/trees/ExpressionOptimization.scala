352010fb52011a90466d152dd62e4bce9b77c5b4
madhu <phatak.dev@gmail.com>
2016-07-25 19:29:20 +0530

    fixed bugs

--- src/main/scala/com/madhukaraphatak/spark/catalyst/trees/ExpressionOptimization.scala
+++ src/main/scala/com/madhukaraphatak/spark/catalyst/trees/ExpressionOptimization.scala
@@ -10,22 +10,23 @@ object ExpressionOptimization {
     lazy val mult2LeafValue = LeafValue(0)
 
     //single level of nesting
-    val multiplicationOptimization = MUL(mult2LeafValue,ADD(mult1LeafValue,mult2LeafValue))
+    val multiplicationOptimization = MUL(mult1LeafValue,MUL(mult1LeafValue,mult2LeafValue))
 
-    val optimizedMultiplication = multiplicationOptimization transform {
+    val transformOptimization = multiplicationOptimization transform {
       case MUL(_,LeafValue(0)) => LeafValue(0)
       case MUL(LeafValue(0),_) => LeafValue(0)
     }
-
-    println(optimizedMultiplication.evaluate)
+    println("transform optimization")
+    println(transformOptimization.numberedTreeString)
 
     //multi level optimization
-    val mul = MUL(mult1LeafValue,MUL(LeafValue(10), LeafValue(0))) transformUp {
+    val transformUpOptimization = MUL(mult1LeafValue,MUL(LeafValue(10), LeafValue(0))) transformUp {
       case MUL(_,LeafValue(0)) => LeafValue(0)
       case MUL(LeafValue(0),_) => LeafValue(0)
     }
 
-    println(mul.evaluate)
+    println("transformUp optimization")
+    println(transformUpOptimization.numberedTreeString)
 
   }
 
--- src/main/scala/com/madhukaraphatak/spark/catalyst/trees/ExpressionTransformation.scala
+++ src/main/scala/com/madhukaraphatak/spark/catalyst/trees/ExpressionTransformation.scala
@@ -10,26 +10,15 @@ object ExpressionTransformation {
     val leafNode2 = new LeafValue(20)
 
     val addNode = ADD(leafNode1,leafNode2)
-
     val complexAddNode = ADD(addNode,LeafValue(50))
-
-    println(addNode.evaluate)
-
-    println(complexAddNode.evaluate)
-
-
     val divideNode = Divide(addNode,LeafValue(0))
 
     println(divideNode.evaluate)
-
     //tree operations
-
     val transformedExpression = divideNode transform   {
       case expression @Divide(_,LeafValue(0)) => LeafValue(Double.NaN)
     }
-
     println(transformedExpression.evaluate)
-
   }
 
 }
